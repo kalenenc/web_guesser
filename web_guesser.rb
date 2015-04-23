24345b3990abdac807adb7b5a2375e2812a9180c
Eleanor Weigert <eweigert@gmail.com>
2015-04-22 17:02:37 -0700

    I3: number guessing game complete

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,23 +1,28 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-secret_num = rand(101)
+SECRET_NUM = rand(101)
 message = "Guess a secret number between 0 and 100!"
 guess_int = nil
 
 get '/' do 
-	erb :index, :locals => {:number => secret_num, :message => message} 
+	erb :index, :locals => {:number => SECRET_NUM, :message => message} 
   if params['guess'] != nil
   	guess_int = params['guess'].to_i
-  	message = 
-  		case guess_int
-  		when secret_num then "You guessed it! The secret number is #{secret_num}"
-  		when (secret_num+5..100) then "WAY too high!"
-			when (secret_num...100) then "Too high!"
-			when (0..secret_num-5) then "WAY too low!"
-			when (0...secret_num) then "Too low!"
-			else message
-  		end 
+  	message = check_guess(guess_int)
 	end
-  erb :index, :locals => {:number => secret_num, :guess => guess_int, :message => message}
+  erb :index, :locals => {:number => SECRET_NUM, :guess => guess_int, :message => message}
 end
+
+def check_guess(guess)
+	case guess
+  	when SECRET_NUM then "You guessed it! The secret number is #{SECRET_NUM}"
+  	when (SECRET_NUM+5..100) then "WAY too high!"
+		when (SECRET_NUM...100) then "Too high!"
+		when (0..SECRET_NUM-5) then "WAY too low!"
+		when (0...SECRET_NUM) then "Too low!"
+		else "pants pants pants"
+ 	end 
+end
+
+
